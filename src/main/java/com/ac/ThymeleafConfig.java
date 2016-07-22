6f915f0a4e53cab1bb3a4abaddbefc76b1755a72
anil1210 <anil@BEANILKU.in.ibm.com>
2016-07-22 13:10:31 +0530

    Thymeleaf configuration added due to angularjs conflicts

---
+++ src/main/java/com/ac/ThymeleafConfig.java
@@ -0,0 +1,30 @@
+package com.ac;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+	@Bean
+	public ServletContextTemplateResolver defaultTemplateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("LEGACYHTML5");
+		resolver.setCacheable(false);
+		return resolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+
+		return messageSource;
+	}
+
+}
