438635356f4bd58e2f9f854edb32a9bae62560c4
anil1210 <anil@BEANILKU.in.ibm.com>
2016-07-22 13:55:32 +0530

    manage the source code addedd @configuration attribute

--- src/main/java/com/ac/MyRoomApplication.java
+++ src/main/java/com/ac/MyRoomApplication.java
@@ -1,13 +1,17 @@
 package com.ac;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ac.controller", "com.ac.model", "com.ac.repository" })
+@Configuration
+@EnableAutoConfiguration
 public class MyRoomApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -18,4 +22,5 @@ public class MyRoomApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MyRoomApplication.class);
 	}
+
 }
