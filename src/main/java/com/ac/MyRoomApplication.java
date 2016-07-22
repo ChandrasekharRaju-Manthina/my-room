e9bdde51696304173feaad47deaa2a2d465dd2ad
anil1210 <anil@BEANILKU.in.ibm.com>
2016-07-22 17:47:26 +0530

    commit the pom changes and added annotaion to configuration file

--- src/main/java/com/ac/MyRoomApplication.java
+++ src/main/java/com/ac/MyRoomApplication.java
@@ -1,11 +1,13 @@
 package com.ac;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ac.controller", "com.ac.model", "com.ac.repository" })
+@EnableAutoConfiguration
 public class MyRoomApplication {
 
 	public static void main(String[] args) {
