a305b4582b56f58fcb5777c1d32818d47f05510f
anil1210 <anil@BEANILKU.in.ibm.com>
2016-07-22 13:48:12 +0530

    Configuration changed

--- src/main/java/com/ac/MyRoomApplication.java
+++ src/main/java/com/ac/MyRoomApplication.java
@@ -2,14 +2,20 @@ package com.ac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.ac.controller", "com.ac.model",
-		"com.ac.repository" })
-public class MyRoomApplication {
+@ComponentScan(basePackages = { "com.ac.controller", "com.ac.model", "com.ac.repository" })
+public class MyRoomApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRoomApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyRoomApplication.class);
 	}
 }
