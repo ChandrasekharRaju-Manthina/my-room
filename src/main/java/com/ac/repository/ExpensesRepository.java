f8faef20623e3239daaf828765b5d58c9be5845a
Chandra <chandra.javadeveloper@gmail.com>
2016-07-22 21:08:38 +0530

    imoprts removed in repository

--- src/main/java/com/ac/repository/ExpensesRepository.java
+++ src/main/java/com/ac/repository/ExpensesRepository.java
@@ -5,7 +5,6 @@ import java.math.BigInteger;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.ac.model.Expense;
-import com.ac.repository.*;
 
 public interface ExpensesRepository extends PagingAndSortingRepository<Expense, BigInteger> {
 
