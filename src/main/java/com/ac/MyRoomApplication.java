352b513c121daf890c06abd1f26e55726553dac4
anil1210 <anil@BEANILKU.in.ibm.com>
2016-07-22 16:00:53 +0530

    added servletContainerCustomer

--- src/main/java/com/ac/MyRoomApplication.java
+++ src/main/java/com/ac/MyRoomApplication.java
@@ -1,9 +1,14 @@
 package com.ac;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +17,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = { "com.ac.controller", "com.ac.model", "com.ac.repository" })
 @Configuration
 @EnableAutoConfiguration
-public class MyRoomApplication extends SpringBootServletInitializer {
+public class MyRoomApplication extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+
+	@Value("${someproperty:webapp/whereever  }")
+	private String documentRoot;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRoomApplication.class, args);
@@ -21,6 +29,11 @@ public class MyRoomApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MyRoomApplication.class);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setDocumentRoot(new File(documentRoot));
 	}
 
 }
