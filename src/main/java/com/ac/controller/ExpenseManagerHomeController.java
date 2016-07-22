d136c2d12fa54380ced96f6b7554eb04a4989641
Chandra <chandra.javadeveloper@gmail.com>
2016-07-22 21:13:20 +0530

    controller committed

--- src/main/java/com/ac/controller/ExpenseManagerHomeController.java
+++ src/main/java/com/ac/controller/ExpenseManagerHomeController.java
@@ -1,20 +1,16 @@
 package com.ac.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class ExpenseManagerHomeController {
 
-	@RequestMapping("/dashBoard")
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+	@RequestMapping(value = "/dashBoard", method = RequestMethod.GET)
+	public String greeting() {
 		return "home";
 	}
-	
-	
 
 }
