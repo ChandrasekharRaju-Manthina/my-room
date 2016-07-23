05775d5adb4d4967407add0f1533e9fee815d45b
Chandra <chandra.javadeveloper@gmail.com>
2016-07-23 20:46:02 +0530

    added serial version UID

--- src/main/java/com/ac/model/Expense.java
+++ src/main/java/com/ac/model/Expense.java
@@ -12,6 +12,11 @@ import javax.persistence.Id;
 @Entity
 public class Expense implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private BigInteger id;
