ba2099d19c96079d48e100b400cd140c4b4f49b9
Chandra <chandra.javadeveloper@gmail.com>
2016-07-24 10:32:42 +0530

    removed unused imports

--- src/main/java/com/ac/MyRoomApplication.java
+++ src/main/java/com/ac/MyRoomApplication.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.sun.glass.ui.Application;
 
 @SpringBootApplication
 @ComponentScan
