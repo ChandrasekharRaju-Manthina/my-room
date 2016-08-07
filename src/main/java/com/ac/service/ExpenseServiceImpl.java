05f2bb79e6feb756f947495f2b713649461a8901
anil1210 <anil@BEANILKU.in.ibm.com>
2016-08-07 10:52:47 +0530

    Added service layer components

---
+++ src/main/java/com/ac/service/ExpenseServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ac.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ac.model.Expense;
+import com.ac.repository.ExpenseRepository;
+
+@Service
+@Transactional
+public class ExpenseServiceImpl implements ExpenseService {
+
+	@Autowired
+	ExpenseRepository expenseRepository;
+
+	@Override
+	public List<Expense> findAll() {
+		return expenseRepository.findAll();
+	}
+
+	
+}
